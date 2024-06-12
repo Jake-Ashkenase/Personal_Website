07ebb76f0b0f9d35af23bd165a4f1210cf01c2d9
Dhruv Kamalesh Kumar <dhruvbaradiya@gmail.com>
2024-06-11 21:50:07 -0400

    updated dependencies

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
-from langchain.document_loaders.csv_loader import CSVLoader
-from langchain.vectorstores import FAISS
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings.openai import OpenAIEmbeddings
 from langchain.prompts import PromptTemplate
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
 
